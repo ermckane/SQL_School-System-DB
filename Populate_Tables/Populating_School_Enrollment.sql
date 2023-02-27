1d0cec0b031cb2db8f09d64a302e02c1d788240e
ermckane <ermckane@gmail.com>
2023-02-26 21:04:40 -0500

    Figured out how to determine age of each student and waht level of school each is in. Now need to group zipcodes for middle/high school and then figure out how to group.

--- Populate_Tables/Populating_School_Enrollment.sql
+++ Populate_Tables/Populating_School_Enrollment.sql
@@ -1,5 +1,37 @@
--- Plan for populating School_Enrollment
--- 1) Take DOB of from each Student in Student_Info and subtract it from current date
+-- Plan for populating School_Enrollment. What pieces I need to fill table
+-- 1) Take DOB of from each Student in Student_Info and subtract it from current date  **SOLVED**
 -- 2) Group zipcodes for middle school and high schools.
--- 3) If they are between certain ages 18-15, they are in High School, 14-12 Middle School, and 11-5 Elementary
+-- 3) If they are between certain ages 18-15, they are in High School, 14-12 Middle School, and 11-5 Elementary  **SOLVED**
 -- 4) Put them in the correct school based off of correpsonding zipcode.
+
+
+/*
+DROP TABLE IF EXISTS #Enrollment_Temp
+;
+
+SELECT si.student_code AS Student_Code, sci.school_code as School_Code, DATEADD(year, 5, si.Birth_Date) as Enrollment_Date
+	FROM Student_Info AS si
+	INNER JOIN School_Info AS sci
+		ON si.ZipCode = sci.Zipcode
+;
+*/
+
+--Calculate age of of students in Student_Info
+SELECT DATEDIFF(year, Birth_Date, GETDATE())
+ + 0 AS Age
+FROM Student_Info
+
+
+--Determine level of school by subtracting date of birth from current date
+SELECT
+CASE 
+	WHEN DATEDIFF(year, Birth_Date, GETDATE()) > 18 THEN 'Graduated'
+	WHEN DATEDIFF(year, Birth_Date, GETDATE()) <= 18 
+		AND DATEDIFF(year, Birth_Date, GETDATE()) >= 15 THEN 'High School'
+	WHEN DATEDIFF(year, Birth_Date, GETDATE()) >= 12
+		AND DATEDIFF(year, Birth_Date, GETDATE()) <= 14 THEN 'Middle School'
+	WHEN DATEDIFF(year, Birth_Date, GETDATE()) >= 5
+		AND DATEDIFF(year, Birth_Date, GETDATE()) <= 12 THEN 'Elementary School'
+	ELSE 'Error'
+	END
+FROM Student_Info
